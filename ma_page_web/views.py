01e2307f3c5227b6a9d348d4eeb2c005438a21d7
SAMANDOULGOU_MOUFTAOouPseudo <mouftaoa@exemple.com>
2026-02-10 17:48:24 +0000

    integrer le message

--- ma_page_web/views.py
+++ ma_page_web/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from .models import Profil, Article, MessageContact
 
 # REMPLACE CECI PAR TA CLÉ COPIÉE (Garde les guillemets)
-resend.api_key = "re_votre_cle_ici" 
+resend.api_key = "re_K3DBWneR_F2SnBtQfRrjgwzwmP4ttZetE" 
 
 def home(request):
     mon_profil = Profil.objects.first()
